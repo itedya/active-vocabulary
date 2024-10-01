ee53ed86c27c0ee525c2c4318b9d1f245ea6b23a
Bartłomiej 'itedya' Domański <bdomanski@domanscy.group>
2024-10-01 10:42:40 +0200

    add frontend of add word form

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -5,9 +5,9 @@ use axum::response::Html as RespondInHtml;
 use axum::response::{IntoResponse, Response};
 use sqlx::{FromRow, PgPool};
 use tracing::log::{log, Level};
-use crate::components::{header, layout, layout_with_basic_wrappers};
+use crate::components::{header, input, layout, layout_with_basic_wrappers, InputType};
 use crate::html::{ClosableHtmlElement, MultipleHtmlElements, RenderableHtmlElement, Text};
-use crate::html::ClosableHtmlElementType::{Button, Div, Form, Main, A};
+use crate::html::ClosableHtmlElementType::{Button, Div, Form, Main, A, P};
 
 fn log_and_return_internal_error(e: impl std::error::Error) -> Response {
     log!(Level::Error, "{}", e);
@@ -57,12 +57,39 @@ pub async fn root(State(pool): State<PgPool>) -> Result<Response, Response> {
 #[debug_handler]
 pub async fn add_word_page(headers: HeaderMap) -> Result<Response, Response> {
     let form = ClosableHtmlElement::new(Form)
+        .with_attribute("hx-post", "/add-word")
+        .with_attribute("hx-target", "main")
         .with_attribute("class", "add-word-form")
         .with_content(
             MultipleHtmlElements::new()
-                .add_element(ClosableHtmlElement::new(Button)
-                    .with_attribute("hx-post", "/add-word")
-                    .with_content(Text::new("Add word")))
+                .add_element(
+                    ClosableHtmlElement::new(Div)
+                        .with_attribute("class", "flex flex-col gap-12")
+                        .with_content(
+                            MultipleHtmlElements::new()
+                                .add_element(input(InputType::Text, "Word", "word", None::<String>))
+                                .add_element(input(InputType::Text, "Translation", "translation", None::<String>))
+                        )
+                )
+                .add_element(
+                    ClosableHtmlElement::new(Div)
+                        .with_attribute("class", "flex-row")
+                        .with_content(
+                            MultipleHtmlElements::new()
+                                .add_element(
+                                    ClosableHtmlElement::new(Button)
+                                        .with_attribute("class", "button-primary")
+                                        .with_attribute("type", "submit")
+                                        .with_content(Text::new("Add word and return to the list"))
+                                )
+                                .add_element(
+                                    ClosableHtmlElement::new(Button)
+                                        .with_attribute("class", "button-secondary")
+                                        .with_attribute("type", "submit")
+                                        .with_content(Text::new("Add word and stay on the page to add another"))
+                                )
+                        )
+                )
         );
 
     if headers.contains_key("HX-Request") {
